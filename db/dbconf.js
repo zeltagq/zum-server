dbd8e063f3c6cd087ce2c9976af7ef385a408270
zeddshaw <zaygo@mail.com>
2020-08-07 23:35:42 +0530

    generalization

--- db/dbconf.js
+++ db/dbconf.js
@@ -3,7 +3,7 @@ const db = require('mongoose');
 db.Promise = global.Promise;
 
 // Production
-db.connect('mongodb+srv://zaygo:5ZPv0F5WAtTxXeOs@zum-dvcvs.mongodb.net/primary?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(
+db.connect('dburl', { useNewUrlParser: true, useUnifiedTopology: true }).then(
     () => { console.log('Connection to database established') }
 ).catch((err) => {
     console.log(err);
